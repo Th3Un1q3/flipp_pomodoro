e23cd8b580ed96d0955009e99bd5704b133ec823
Mikhail G <mg@rademade.com>
2023-01-08 10:19:36 +0100

    add missing params docs

--- flipp_pomodoro/modules/flipp_pomodoro.h
+++ flipp_pomodoro/modules/flipp_pomodoro.h
@@ -3,6 +3,7 @@
 #include <furi_hal.h>
 #include "../helpers/time.h"
 
+/// @brief Options of pomodoro stages
 typedef enum {
     Work,
     Rest,
@@ -15,6 +16,7 @@ typedef struct {
 } FlippPomodoroState;
 
 /// @brief Generates initial state
+/// @param state - pointer to the state of pomorodo.
 /// @returns A new pre-populated state for pomodoro timer
 FlippPomodoroState flipp_pomodoro__new();
 
@@ -22,18 +24,22 @@ FlippPomodoroState flipp_pomodoro__new();
 void flipp_pomodoro__destroy(FlippPomodoroState* state);
 
 /// @brief Get remaining stage time.
+/// @param state - pointer to the state of pomorodo.
 /// @returns Time difference to the end of current stage
 TimeDifference flipp_pomodoro__stage_remaining_duration(FlippPomodoroState* state);
 
 /// @brief Label of transition to the next stage
+/// @param state - pointer to the state of pomorodo.
 /// @returns string with the label of the "skipp" button
 char* flipp_pomodoro__next_stage_label(FlippPomodoroState* state);
 
 /// @brief Check if current stage is expired
+/// @param state - pointer to the state of pomorodo.
 /// @returns expriations status - true means stage is expired
 bool flipp_pomodoro__is_stage_expired(FlippPomodoroState* state);
 
 /// @brief Rotate stage of the timer
+/// @param state - pointer to the state of pomorodo.
 void flipp_pomodoro__toggle_stage(FlippPomodoroState* state);
 
 
